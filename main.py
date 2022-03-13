4d253c57e45d68d44a1293329a5db0c19224a247
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2022-03-13 17:36:25 +0100

    commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 if __name__ == "__main__":
 
    from clases.insercion_dicotomica import *
-
+   print("El ejercicio 1:")
+   print("Primer apartado:")
+   tabla = [3,6,2,9,4,8,1,5,7,0]
+   print(tabla)
+   print("La tabla ordenada por dicotomia es: {}")
 
 
 
