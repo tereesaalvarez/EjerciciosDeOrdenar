680202248fd833fb9b3e00e600047b9ea7cdee7b
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2022-03-13 16:27:18 +0000

    commit

--- clases/ordenacion_topológica.py
+++ clases/ordenacion_topológica.py
@@ -4,6 +4,8 @@ class lista:
 
     def ordenacion_burbuja(self):
       for i in range(len(self.lista)-1):
-
-
+        for j in range(len(self.lista)-1):
+          if self.lista[j] > self.lista[j+1]:
+            print("Intercambiamos {} con {}".format(self.lista[j], self.lista[j+1]))
+            self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
       return self.lista
