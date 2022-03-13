28f913259a77029d7573d3bae245921ecc47abf7
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2022-03-13 16:28:53 +0100

    commit

--- clases/insercion_dicotomica.py
+++ clases/insercion_dicotomica.py
@@ -1,9 +1,17 @@
 class dicotomia1:
     def __init__(self):
-        self = tabla
+        self.tabla = tabla
 
     def ordenar(self):
-        self
-
+        self.tabla.sort()
+        return self.tabla
 
 class dicotomia2:
+    def __init__(self):
+        r=[]
+        self=r
+    
+    def ordenar(self):
+        for i in self.tabla:
+            self.append()
+        return selfc
