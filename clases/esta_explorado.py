aa7bd8f47f953abc45fcc41a89214017b7994d8c
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-13 18:41:59 +0100

    commit

--- clases/esta_explorado.py
+++ clases/esta_explorado.py
@@ -1,2 +1,14 @@
-class segmento_de_tabla:
-    def __init__(self,)
+class segmento_de_tablas:
+
+    def __init__(self,tabla):
+        self.tabla = tabla
+
+    def segmentos(self):
+        r=[]
+        for i in range(len(self.tabla)-1):
+            for j in range(len(self.lista)-1):
+            if self.lista[j] > self.lista[j+1]:
+           
+            print("Intercambiamos {} con {}".format(self.lista[j], self.lista[j+1]))
+            self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
+      return self.lista
