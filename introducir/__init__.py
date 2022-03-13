6a95568e0aaf98f1e78244aaab7aa3688010ed84
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2022-03-13 16:09:06 +0000

    init introducir

---
+++ introducir/__init__.py
@@ -0,0 +1,21 @@
+"""
+Módulo que gestiona todas las funcionalidades de introducción de datos
+"""
+
+from .numero import (
+    solicitar_introducir_numero,
+    solicitar_introducir_numero_extremo,
+)
+
+from .buleano import (
+    solicitar_introducir_si_o_no,
+    solicitar_introducir_verdadero_o_falso,
+)
+
+from .cadena import (
+    solicitar_introducir_cadena,
+    solicitar_introducir_char,
+    solicitar_introducir_letra,
+    solicitar_introducir_palabra,
+    solicitar_introducir_casilla,
+)
